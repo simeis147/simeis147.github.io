a6aa6a4422999921abdc5a5b33e190fabc58229d
simeis147 <cpdong7@gmail.com>
2023-10-17 06:55:43 +0000

    Deploying to gh-pages from @ simeis147/simeis147.github.io@5de36e2bd6b0ccdfdb44f33260736f029648b779 🚀

---
+++ assets/pageview-0dda297d.js
@@ -0,0 +1,1 @@
+import{ab as e}from"./app-a8e5c4d2.js";const t=()=>e({serverURL:"https://test-commit-six.vercel.app/"});export{t as updatePageview};
--- assets/pageview-c0d5273d.js
+++
@@ -1,1 +0,0 @@
-import{ab as e}from"./app-e1448041.js";const t=()=>e({serverURL:"https://test-commit-six.vercel.app/"});export{t as updatePageview};
